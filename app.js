d16a67ec5cce58241a4ba7b5333ae3a9b325a535
Adam Cessna <racessna@gmail.com>
2020-09-17 14:26:42 -0500

    Require dependencies and get Express server up and running. Add Genres array of objects

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
-const app = require('express');
+const express = require('express');
 const Joi = require('joi');
 
+const app = express();
+
+app.use(express.json());
+
+let genres = [
+    {
+        name: "horror",
+        id: 1
+    },
+    {
+        name: "romcom",
+        id: 1
+    },
+    {
+        name: "comedy",
+        id: 1
+    }
+]
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
